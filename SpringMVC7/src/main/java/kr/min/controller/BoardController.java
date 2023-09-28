6ae987657689570583ebbc9d31ac811937fb4efc
minzzweb <minzzweb@gmail.com>
2023-09-28 23:32:48 +0900

    list

--- SpringMVC7/src/main/java/kr/min/controller/BoardController.java
+++ SpringMVC7/src/main/java/kr/min/controller/BoardController.java
@@ -4,18 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.min.entity.Board;
 import kr.min.service.BoardService;
 
 @Controller //POJO
+@RequestMapping("/board/*")
 public class BoardController {
  
 	@Autowired
-	BoardService service;
+	BoardService boardService;
 	
-//	public List<Board> getList(){
-//		
-//		return 
-//	}
+	@GetMapping("/list")
+	public String getList(Model model){
+		
+		 List<Board> list =  boardService.getList();
+		 
+		 //객체바인딩
+		 model.addAttribute("list",list);
+		
+		return "board/boardList";
+	}
 }
