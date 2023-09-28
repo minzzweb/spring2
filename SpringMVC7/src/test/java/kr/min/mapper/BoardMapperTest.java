695780c154c8397475ea294ce218dc38d66b3444
minzzweb <minzzweb@gmail.com>
2023-09-28 22:22:42 +0900

    db list test

---
+++ SpringMVC7/src/test/java/kr/min/mapper/BoardMapperTest.java
@@ -0,0 +1,30 @@
+package kr.min.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.min.entity.Board;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BoardMapperTest {
+
+	@Autowired 
+	BoardMapper boardMapper;
+	
+	@Test
+	public void testGetList() {
+		List<Board> list = boardMapper.getList();
+		
+		for(Board vo : list) {
+			System.out.println(vo);
+		}
+	}
+}
