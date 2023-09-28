f94b0702a9877132ab08d5fa7f7b0cc5c7e5066a
minzzweb <minzzweb@gmail.com>
2023-09-28 22:21:56 +0900

    게시판 목록 mapper추가

--- SpringMVC7/src/main/java/kr/min/mapper/BoardMapper.java
+++ SpringMVC7/src/main/java/kr/min/mapper/BoardMapper.java
@@ -1,6 +1,12 @@
 package kr.min.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.min.entity.Board;
+
 //@Mapper
 public interface BoardMapper {
-
+	public List<Board> getList();
 }
--- SpringMVC7/target/classes/kr/min/entity/db.sql
+++ SpringMVC7/target/classes/kr/min/entity/db.sql
@@ -14,4 +14,44 @@ create table tblBoard(
 	primary key(idx)
 );
 
+select max(idx) from tblBoard; -- NULL -> 1
+select IFNULL(max(idx)+1,1) from tblBoard;
+select IFNULL(max(boardGroup)+1,0) from tblBoard; -- NULL -> 0
 
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'bit01','게시판연습','게시판연습','관리자',
+now(),0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'bit02','게시판연습','게시판연습','박매일',
+now(),0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'bit03','게시판연습','게시판연습','홍길동',
+now(),0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+select * from tblBoard;
+
+-- 회원테이블 --
+create table tblMember(
+  memID varchar(50) not null, -- 회원ID
+  memPwd varchar(50) not null, -- 회원비번
+  memName varchar(50) not null, -- 회원이름
+  memPhone varchar(50) not null, -- 회원전화번호
+  memAddr varchar(100), -- 회원주소
+  latitude decimal(13,10), -- 현재위치위도
+  longitude decimal(13,10), -- 현재위치경도
+  primary key(memID)
+);
+
+insert into tblMember(memID, memPwd, memName, memPhone)
+	values('bit01','bit01','관리자','010-1111-1111');
+insert into tblMember(memID, memPwd, memName, memPhone)
+	values('bit02','bit02','박매일','010-2222-2222');
+insert into tblMember(memID, memPwd, memName, memPhone)
+	values('bit03','bit03','홍길동','010-3333-3333');
+	
+select * from tblMember;
